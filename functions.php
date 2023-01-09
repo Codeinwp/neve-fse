0a8969fadea12ccc0715ce374bee48d3ff9cffc7
abaicus <baicusandrei@me.com>
2023-01-09 18:00:58 +0200

    Add child theme files

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+define( 'NEVE_FSE_MODE', true );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+if ( ! function_exists( 'neve_child_load_css' ) ):
+	/**
+	 * Load CSS file.
+	 */
+	function neve_child_load_css() {
+		wp_enqueue_style( 'neve-child-style', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'neve-style' ), NEVE_VERSION );
+	}
+endif;
+add_action( 'wp_enqueue_scripts', 'neve_child_load_css', 20 );
+
