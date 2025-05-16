dc229287636f977819185ebdff2701813775e49d
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2025-05-16 15:55:25 +0300

    feat: add Black Friday integration

--- inc/Admin.php
+++ inc/Admin.php
@@ -68,6 +68,7 @@ class Admin {
 		add_action( 'wp_ajax_neve_fse_dismiss_design_pack_notice', array( $this, 'remove_design_pack_notice' ) );
 		add_action( 'activated_plugin', array( $this, 'after_otter_activation' ) );
 		add_action( 'wp_ajax_neve_fse_set_otter_ref', array( $this, 'set_otter_ref' ) );
+		add_filter( 'themeisle_sdk_blackfriday_data', array( $this, 'add_black_friday_data' ), 20 );
 	}
 
 	/**
@@ -560,5 +561,32 @@ class Admin {
 			2 
 		);
 		do_action( 'themeisle_internal_page', NEVE_FSE_PRODUCT_SLUG, $screen->id );
+	}
+
+	/**
+	 * Add Black Friday data.
+	 *
+	 * @param array $configs The configuration array for the loaded products.
+	 *
+	 * @return array
+	 */
+	public function add_black_friday_data( $configs ) {
+		$config = $configs['default'];
+
+		// translators: %1$s - plugin namce, %2$s - HTML tag, %3$s - discount, %4$s - HTML tag, %5$s - company name.
+		$message_template = __( 'Enhance %1$s with %2$s– up to %3$s OFF in our biggest sale of the year. Limited time only.', 'neve-fse' );
+
+		$config['dismiss']  = true; // Note: Allow dismiss since it appears on `/wp-admin`.
+		$config['message']  = sprintf( $message_template, 'Neve FSE', 'Otter Blocks Pro', '70%' );
+		$config['sale_url'] = add_query_arg(
+			array(
+				'utm_term' => 'free',
+			),
+			tsdk_translate_link( tsdk_utmify( 'https://themeisle.link/otter-bf', 'bfcm', 'neve-fse' ) )
+		);
+
+		$configs[ NEVE_FSE_PRODUCT_SLUG ] = $config;
+
+		return $configs;
 	}
 }
